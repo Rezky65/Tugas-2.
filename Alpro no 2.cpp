f2459519df1d6878d6c0003113dde502cbe7c4d6
Rezky65 <62123036+Rezky65@users.noreply.github.com>
2020-03-31 15:49:22 +0700

    Add files via upload

---
+++ Alpro no 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int i,j,k,a;
+	int dus[3][3]={(2,4,6),(8,10,12),(14,16,18)};
+	
+	cout<<"jumlah dus = 9"<<"\n";
+	
+	cout<<"nomor dus = ";
+	for(i=1;i<=9;i++){
+		cout<<i*2<<" ";
+	}
+	cout<<endl;
+	for(i=2;i<=2;i++){
+		cout<<"Nomor dus 1 = "<<i<<"\n";
+		cout<<"Nomor dus 2 = "<<i*2<<"\n";
+		cout<<"Nomor dus 3 = "<<i*3<<"\n";
+		cout<<"Nomor dus 4 = "<<i*4<<"\n";
+		cout<<"Nomor dus 5 = "<<i*5<<"\n";
+		cout<<"Nomor dus 6 = "<<i*6<<"\n";
+		cout<<"Nomor dus 7 = "<<i*7<<"\n";
+		cout<<"Nomor dus 8 = "<<i*8<<"\n";
+		cout<<"Nomor dus 9 = "<<i*9<<"\n";
+	}
+	
+}
